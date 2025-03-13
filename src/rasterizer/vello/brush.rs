7543f7ce14760fea09a3278e67a52ba259d31e32
Joshua Thijssen <jthijssen@noxlogic.nl>
2025-03-13 10:51:24 +0100

    tmp

--- src/rasterizer/vello/brush.rs
+++ src/rasterizer/vello/brush.rs
@@ -1,17 +1,17 @@
-use vello::peniko::{Blob, BrushRef};
+use vello::peniko::{Blob, Brush as VelloBrush};
 use vello::peniko::color::{AlphaColor, Rgba8};
 use crate::common::geo::Rect;
 use crate::painter::commands::brush::Brush;
 use vello::peniko::Image as PenikoImage;
 
-pub fn set_brush(brush: &Brush, _rect: Rect) -> BrushRef {
+pub fn set_brush(brush: &Brush, _rect: Rect) -> VelloBrush {
     match brush {
         Brush::Solid(color) => {
             let c = Rgba8::from_u8_array([color.r8(), color.g8(), color.b8(), color.a8()]);
-            BrushRef::Solid(AlphaColor::from(c))
+            VelloBrush::Solid(AlphaColor::from(c))
         }
         Brush::Image(img) => {
-            BrushRef::Image(&PenikoImage::new(
+            VelloBrush::Image(PenikoImage::new(
                 Blob::from(img.data().to_vec()),
                 vello::peniko::ImageFormat::Rgba8,
                 img.width(),
--- src/rasterizer/vello/rectangle.rs
+++ src/rasterizer/vello/rectangle.rs
@@ -1,122 +1,46 @@
 use vello::kurbo;
-use vello::kurbo::{Affine, Rect, RoundedRect};
-use vello::peniko::Fill;
+use vello::kurbo::{Affine, Rect, RoundedRect, Shape};
+use vello::peniko::{Fill};
 use crate::painter::commands::border::BorderStyle;
 use crate::painter::commands::rectangle::Rectangle;
 use crate::rasterizer::vello::brush::set_brush;
 use crate::tiler::Tile;
 
-pub(crate) fn do_paint_rectangle(scene: &mut vello::Scene, tile: &Tile, rectangle: &Rectangle) {
+pub(crate) fn do_paint_rectangle(scene: &mut vello::Scene, _tile: &Tile, rect: &Rectangle) {
     // // Translate the context to the tile's position and clip it.
     // cr.translate(-tile.rect.x, -tile.rect.y);
     // cr.rectangle(tile.rect.x, tile.rect.y, tile.rect.width, tile.rect.height);
     // cr.clip();
 
-    // Create initial rect
-    match rectangle.background() {
+    // Draw background (if any background brush is defined)
+    match rect.background() {
         Some(brush) => {
-            let vello_rect = setup_rectangle_path(rectangle);
-            let vello_brush = set_brush(brush, rectangle.rect());
+            let vello_rect = setup_rectangle_path(rect);
+            let vello_brush = set_brush(brush, rect.rect());
 
-            let s =
-
+            let vello_shape = vello_rect.get_shape();
             scene.fill(
                 Fill::NonZero,
                 Affine::IDENTITY,
-                vello_brush,
+                &vello_brush,
                 None,
-                &vello_rect,
+                &vello_shape,
             );
         }
         None => {}
     }
 
     // Create border
-    let vello_rect = setup_rectangle_path(rectangle);
-    let vello_brush = set_brush(&rectangle.border().brush(), rectangle.rect());
-
-    match rectangle.border().style() {
-        BorderStyle::None => {
-            // No border
-        }
-        BorderStyle::Solid => {
-            // Complete solid line
-            scene.stroke(
-                &kurbo::Stroke::new(rectangle.border().width() as f64).with_dashes(0.0, vec![]),
-                Affine::IDENTITY,
-                &vello_brush,
-                None,
-                &vello_rect,
-            );
-        }
-        BorderStyle::Dashed => {
-            // 50px dash, 10px gap, 10px dash, 10px gap
-            scene.stroke(
-                &kurbo::Stroke::new(rectangle.border().width() as f64).with_dashes(0.0, &[50.0, 10.0, 10.0, 10.0]),
-                Affine::IDENTITY,
-                &vello_brush,
-                None,
-                &vello_rect,
-            );
-        }
-        BorderStyle::Dotted => {
-            // 10px dash, 10px gap
-            scene.stroke(
-                &kurbo::Stroke::new(rectangle.border().width() as f64).with_dashes(0.0, &[10.0, 10.0]),
-                Affine::IDENTITY,
-                &vello_brush,
-                None,
-                &vello_rect,
-            );
-        }
-        BorderStyle::Double => {
-            if rectangle.border().width() >= 3.0 {
-                // The formula  outer border: (N-1) / 2, 1px gap, inner border: (N-1) / 2
-
-                // Outer border
-                let width = (rectangle.border().width() / 2.0).floor();
-                scene.stroke(
-                    &kurbo::Stroke::new(width as f64),
-                    Affine::IDENTITY,
-                    &vello_brush,
-                    None,
-                    &vello_rect,
-                );
-
-                let gap_size = 1.0;
-
-                // inner border
-                let inner_border_rect = Rect::new(
-                    rectangle.rect().x + width as f64 + gap_size,
-                    rectangle.rect().y + width as f64 + gap_size,
-                    rectangle.rect().width - width as f64 - gap_size,
-                    rectangle.rect().height - width as f64 - gap_size
-                );
-                scene.stroke(
-                    &kurbo::Stroke::new(width as f64),
-                    Affine::IDENTITY,
-                    &vello_brush,
-                    None,
-                    &inner_border_rect,
-                );
-
-            } else {
-                // When the width is less than 3.0, we just draw a single line as there is no room for
-                // a double border
-                scene.stroke(
-                    &kurbo::Stroke::new(rectangle.border().width() as f64),
-                    Affine::IDENTITY,
-                    &vello_brush,
-                    None,
-                    &vello_rect,
-                );
-
-            }
-        }
-        BorderStyle::Groove => {}
-        BorderStyle::Ridge => {}
-        BorderStyle::Inset => {}
-        BorderStyle::Outset => {}
+    match rect.border().style() {
+        BorderStyle::None => {},
+        BorderStyle::Solid => draw_single_border(scene, rect, vec![]),
+        BorderStyle::Dashed => draw_single_border(scene, rect, vec![50.0, 10.0, 10.0, 10.0]),
+        BorderStyle::Dotted => draw_single_border(scene, rect, vec![10.0, 10.0]),
+        BorderStyle::Double => draw_double_border(scene, rect),
+        BorderStyle::Groove => { unimplemented!() }
+        BorderStyle::Ridge => { unimplemented!() }
+        BorderStyle::Inset => { unimplemented!() }
+        BorderStyle::Outset => { unimplemented!() }
         BorderStyle::Hidden => {
             // Don't display anything. But the border still takes up space. This is already
             // calculated in the box model by the layouter.
@@ -124,9 +48,80 @@ pub(crate) fn do_paint_rectangle(scene: &mut vello::Scene, tile: &Tile, rectangl
     }
 }
 
+fn draw_single_border(scene: &mut vello::Scene, rect: &Rectangle, dashes: Vec<f64>) {
+    let vello_shape = setup_rectangle_path(rect);
+    let vello_brush = set_brush(&rect.border().brush(), rect.rect());
+    let vello_stroke = kurbo::Stroke::new(rect.border().width() as f64).with_dashes(0.0, dashes);
+
+    scene.stroke(
+        &vello_stroke,
+        Affine::IDENTITY,
+        &vello_brush,
+        None,
+        &vello_shape,
+    );
+}
+
+fn draw_double_border(scene: &mut vello::Scene, rect: &Rectangle) {
+    let vello_shape = setup_rectangle_path(rect);
+    let vello_brush = set_brush(&rect.border().brush(), rect.rect());
+
+    if rect.border().width() < 3.0 {
+        // When the width is less than 3.0, we just draw a single line as there is no room for
+        // a double border
+        scene.stroke(
+            &kurbo::Stroke::new(rect.border().width() as f64),
+            Affine::IDENTITY,
+            &vello_brush,
+            None,
+            &vello_shape,
+        );
+
+        return;
+    }
+
+    // The formula: outer border: (N-1) / 2, 1px gap, inner border: (N-1) / 2
+
+    // Outer border
+    let width = (rect.border().width() / 2.0).floor();
+    scene.stroke(
+        &kurbo::Stroke::new(width as f64),
+        Affine::IDENTITY,
+        &vello_brush,
+        None,
+        &vello_shape,
+    );
+
+    let gap_size = 1.0;
+
+    // inner border
+    let inner_border_rect = Rect::new(
+        rect.rect().x + width as f64 + gap_size,
+        rect.rect().y + width as f64 + gap_size,
+        rect.rect().width - width as f64 - gap_size,
+        rect.rect().height - width as f64 - gap_size
+    );
+    scene.stroke(
+        &kurbo::Stroke::new(width as f64),
+        Affine::IDENTITY,
+        &vello_brush,
+        None,
+        &inner_border_rect,
+    );
+}
+
 enum RectType {
     RoundedRect(RoundedRect),
     Rect(Rect),
+}
+
+impl RectType {
+    pub(crate) fn get_shape(&self) -> &impl Shape {
+        match self {
+            RectType::RoundedRect(r) => r,
+            RectType::Rect(r) => r,
+        }
+    }
 }
 
 /// Creates a cairo rectangle with either sharp or rounded corners. Does not fill or stroke the path.
